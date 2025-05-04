e40b13ac9176841b1f86f568616d1cd35ab131aa
vasu-636 <vasupatel632006@gmail.com>
2025-05-04 10:42:17 +0530

    portfolio completed

---
+++ assets/js/counter.js
@@ -0,0 +1,37 @@
+
+$.fn.jQuerySimpleCounter = function( options ) {
+    var settings = $.extend({
+        start:  0,
+        end:    100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options );
+
+    var thisElement = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function() {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+
+$('#n1').jQuerySimpleCounter({end: 232,duration: 2000});
+$('#n2').jQuerySimpleCounter({end: 521,duration: 2000});
+$('#n3').jQuerySimpleCounter({end: 1432,duration: 2000});
+$('#n4').jQuerySimpleCounter({end: 15,duration: 2000});
+
+
+
+  /* AUTHOR LINK */
+ $('.about-me-img').hover(function(){
+        $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+    }, function(){
+        $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+    });
---
+++ assets/js/headline.js
@@ -0,0 +1,40 @@
+//you can change the text, but you need to keep same format [yourtext, color];
+let yourtext = [
+    ["Developer", "#18d26e"],
+    ["Designer", "#18d26e"],
+    ["Account Manager", "#18d26e"]
+  ];
+  
+  let x = 0;
+  let y = 0;
+  //how fast typing is
+  let wait = 200;
+  //how long you want to text stay before overwriting
+  let additionalwait = 5;
+  let txt = yourtext[0][0].split("");
+  let out = "";
+  let retyping = setInterval(function () {
+    document.getElementById("changingText").innerHTML = out;
+    if (x > txt.length - 1) {
+    } else {
+      out += txt[x];
+    }
+    x++;
+    if (x == txt.length + 2 + additionalwait) {
+      if (y == yourtext.length - 1) {
+        y = 0;
+        txt = yourtext[y][0].split("");
+        out = "";
+        document.getElementById("changingText").innerHTML = out;
+        document.getElementById("changingText").style.color = yourtext[y][1];
+        x = 0;
+      } else {
+        y += 1;
+        txt = yourtext[y][0].split("");
+        out = "";
+        document.getElementById("changingText").innerHTML = out;
+        document.getElementById("changingText").style.color = yourtext[y][1];
+        x = 0;
+      }
+    }
+  }, wait);
